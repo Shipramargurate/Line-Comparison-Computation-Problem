d97aa34b1fd8e723cfe5bead6094a36f7ee69fec
shipramargurate <shivanikumari22322@gmail.com>
2022-09-23 19:58:14 +0530

    Added : Functionality of calculating the length of a line

--- src/com/brideglabz/linecomp/LineComparison.java
+++ src/com/brideglabz/linecomp/LineComparison.java
@@ -2,10 +2,15 @@ package com.brideglabz.linecomp;
 
 public class LineComparison {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Welcome to Line Comparison Computation Program...!");
+	// UC-1 - Taking coordinates and calculating the length of a line
 
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO LINE COMPARISON COMPUTATION PROGRAM....!!");
+		int x1 = 20, y1 = 70;
+		int x2 = 9, y2 = 67;
+
+		double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println("Length of Line : " + length);
 	}
 
 }
